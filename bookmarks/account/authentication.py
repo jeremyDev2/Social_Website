c499800d66fdda1f3f2624d68be2752cd117069f
Daniel <danil.malaev3@gmail.com>
2026-02-12 17:52:35 +0200

    create_profile func

--- bookmarks/account/authentication.py
+++ bookmarks/account/authentication.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from account.models import Profile
 
 class EmailAuthBackend:
     
@@ -16,3 +17,7 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+def create_profile(user, backend, *args, **kwargs):
+    Profile.objects.get_or_create(user=user)
+    return {'user': user}
