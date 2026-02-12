955c4688fbc858cfe7440ab9ffab4f9bda386849
Daniel <danil.malaev3@gmail.com>
2026-02-12 23:00:28 +0200

    fix syntax

--- bookmarks/account/admin.py
+++ bookmarks/account/admin.py
@@ -4,4 +4,4 @@ from .models import Profile
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user','date_of_birth', 'photo']
-    raw_id_fields = ['user'] 
+    raw_id_fields = ['user']
