9dada5d9d901b61c844712c133f40e93e7312e71
Daniel <danil.malaev3@gmail.com>
2026-02-18 23:24:34 +0200

    add: 'user_follow' handler

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,9 +1,10 @@
-from django.contrib.auth import authenticate, login, get_user_model
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse, JsonResponse
+from .models import Profile, Contact
+from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
 def user_login(request):
@@ -76,3 +77,20 @@ def user_list(request):
 def user_detail(request, username):
     user = get_object_or_404(User, username=username, is_active=True)
     return render(request, 'account/user/detail.html', {'section':'people', 'user':user})
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == "follow":
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({"status": "ok"})
+        except User.DoesNotExist:
+            return JsonResponse({"status": "error"})
+    return JsonResponse({"status": "error"})
