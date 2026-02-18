9b17b01daaf026a2f38d7dbb625a40ac55c7828b
Daniel <danil.malaev3@gmail.com>
2026-02-18 14:34:21 +0200

    'user_detail'&'user_list' handlers

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
@@ -65,4 +65,14 @@ def edit(request):
     return render(request, 'account/edit.html',{'user_form':user_form, 'profile_form': profile_form})
 
 
+User = get_user_model()
 
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request, 'account/user/list.html', {'section':'people', 'user': users})
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(request, 'account/user/detail.html', {'section':'people', 'user':user})
