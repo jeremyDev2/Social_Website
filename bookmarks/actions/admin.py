b0a09bb24c5963363d78ff257ea4c88ecf61941c
Daniel <danil.malaev3@gmail.com>
2026-02-19 13:25:31 +0200

    added Action model to admin file

--- bookmarks/actions/admin.py
+++ bookmarks/actions/admin.py
@@ -1,3 +1,8 @@
+from .models import Action
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'verb', 'target', 'created']
+    list_filter = ['created']
+    search_fields = ['verb']
