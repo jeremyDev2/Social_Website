85f6a61868e9fe87d3c69394d3d39c743bc49b41
Daniel <danil.malaev3@gmail.com>
2026-02-19 15:20:00 +0200

    update: using QuerySet method to retrieve related obj.

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -37,7 +37,7 @@ def dashboard(request):
     if following_ids:
         #if user is following others, retrieve only their actions
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions[:10]
+    actions = actions.select_related('user', 'user__profile')[:10]
     return render(
         request, 'account/dashboard.html', {'section':'dashboard','actions':actions}
     )
