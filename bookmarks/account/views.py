d1c5cc8d1fc06987759b1ec4b858fd3af23e1734
Daniel <danil.malaev3@gmail.com>
2026-02-19 14:41:20 +0200

    add: action func. to handlers

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,10 +1,11 @@
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from django.views.decorators.http import require_POST
 from django.http import HttpResponse, JsonResponse
 from .models import Profile, Contact
 from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
+from actions.utils import create_action
 from django.contrib import messages
 
 def user_login(request):
@@ -36,13 +37,19 @@ def register(request):
     if request.method == "POST":
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
+
             #create new user obj. but not saving it yet
             new_user = user_form.save(commit=False)
+
             #set choosen password
             new_user.set_password(user_form.cleaned_data['password'])
+
             #save the User obj.
             new_user.save()
+
+            # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,'account/register_done.html', {'new_user':new_user})
     else:
         user_form = UserRegistrationForm()
@@ -88,6 +95,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == "follow":
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({"status": "ok"})
