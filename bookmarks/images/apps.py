bd720e36577077b783d6dc0358f61d32a7b837c8
Daniel <danil.malaev3@gmail.com>
2026-02-20 12:33:14 +0200

    add: ready() function for starting signals

--- bookmarks/images/apps.py
+++ bookmarks/images/apps.py
@@ -2,4 +2,10 @@ from django.apps import AppConfig
 
 
 class ImagesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'images'
+    
+    #called when the app is initialized
+    def ready(self):
+        import signal handlers
+        import images.signals
