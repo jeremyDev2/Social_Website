00cd9d41e8643651d69d8af0fad9046e2776832b
Daniel <danil.malaev3@gmail.com>
2026-02-20 14:59:10 +0200

    add: connection to Redis &  increment functionality in 'image_detail' handler

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -7,6 +7,14 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from actions.utils import create_action
 from django.contrib import messages
+from django.conf import settings
+import redis
+
+# connect to redis
+r = redis.Redis(host=settings.REDIS_HOST, 
+                port=settings.REDIS_PORT, 
+                db=settings.REDIS_DB)
+
 
 @login_required
 def image_create(request):
@@ -31,7 +39,11 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+    total_views = r.incr(f'image:{image.id}:views')
+    
+    #increment image ranking by 1 (https://redis.io/docs/latest/commands/zincrby/)
+    r.zincrby('image_ranking', 1, image.id)
+    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image, 'total_views': total_views})
 
 @login_required
 @require_POST
